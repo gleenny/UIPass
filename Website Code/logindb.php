51675511b5e7bcaa96f0826f215b3af3d8e0a57f
c-a-e-z-a-r <152083162+c-a-e-z-a-r@users.noreply.github.com>
2024-04-09 15:52:57 +0800

    logindb.php

---
+++ Website Code/logindb.php
@@ -0,0 +1,39 @@
+<?php
+session_start(); // Start the session
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $host = "localhost:3307"; //problema to if online na
+    $dbusername = "root";
+    $dbpassword = "";
+    $dbname = "iupassdb";
+
+    $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $query = "SELECT * FROM accounttbl WHERE username ='$username' AND password ='$password'";
+
+    $result = $conn->query($query);
+
+    if ($result->num_rows == 1) {
+        header("Location: Dashboard.html");
+        exit();
+    } else {
+        $_SESSION['message'] = "Incorrect User Name or Password";
+        header("Location: index.php");
+        exit();
+    }
+
+    $conn->close();
+} else {
+    header("Location: index.php");
+    echo "did not login properly";
+}
+?>
